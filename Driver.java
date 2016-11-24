4b374496c6d82c39c4e0a3641558ddb9e3636ea1
MaroGrigoryan <maro.grigoryan@inbox.ru>
2016-11-24 21:14:46 +0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -5,11 +5,11 @@ public class Driver {
 	public static void main(String[] args) {
 		Square box = new Square(100);
 		Rectangle rectangle = new Rectangle(50,80);
-		
+		double d=15.6;
 		System.out.println(box.getArea());
 		System.out.println(rectangle.getArea());
-		System.out.println(Math.factorial((int)15.6));
-		System.out.println(Math.factorialLoop((int)15.6));
+		System.out.println(Math.factorial((int)d));
+		System.out.println(Math.factorialLoop((int)d));
 		
 	}
 	
